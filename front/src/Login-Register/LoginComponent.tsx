8705201504760ee223697a9a76b2d301593bacc1
Myroslav Torbiak <2172695@teb-gdansk.ad>
2023-05-17 08:26:34 +0200

    add login

---
+++ front/src/Login-Register/LoginComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {LoginInput} from "./LoginStyle";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+const Login = () => {
+    return (
+        <div>
+            <LoginInput/>
+        </div>
+    );
+};
+
+export default Login;
---
+++ front/src/Login-Register/LoginStyle.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const LoginInput = styled.input`
+  background: darkred;
+  border: none;
+`
