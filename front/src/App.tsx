9deb935e7ad71c2b91b14d6075c8595f2a05b352
Myroslav Torbiak <2172695@teb-gdansk.ad>
2023-05-19 10:33:44 +0200

    1

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,15 +4,19 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./Login-Register/LoginComponent";
 import Register from "./Login-Register/RegisterComponent";
 import Home from "./Home/HomeComponent";
+import {GlobalStyle} from "./globalStyle";
+import StartPage from "./StartPage/StartPageComponent";
 
 function App() {
   return (
     <div className="App">
         <BrowserRouter>
+            <GlobalStyle/>
             <Routes>
-                <Route element={<Home/>} path={"/"}/>
+                <Route element={<StartPage/>} path={"/"}/>
                 <Route element={<Login/>} path={"/login"}/>
                 <Route element={<Register/>} path={"/register"}/>
+                <Route element={<Home/>} path={"/home"}/>
             </Routes>
         </BrowserRouter>
     </div>
--- front/src/Login-Register/RegisterComponent.tsx
+++ front/src/Login-Register/RegisterComponent.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {LoginInput} from "./LoginStyle";
 
 const Register = () => {
+    const [typePassword, setTypePassword] = useState("password")
+
+    const ShowPassword = () => {
+        if (typePassword == "password"){
+            setTypePassword("text")
+        }
+        else {
+            setTypePassword("password")
+        }
+    }
     return (
         <div>
-            
+            <LoginInput placeholder={"Name"}/>
+            <LoginInput placeholder={"Surname"}/>
+            <LoginInput placeholder={"Password"} type={typePassword}/>
+            <button onClick={ShowPassword}>show </button>
+            <LoginInput placeholder={"repeat password"} type={typePassword}/>
+            <LoginInput placeholder={"e-mail"}/>
+
         </div>
     );
 };
